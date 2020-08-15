3713231111c02d590819665fdc5dbd8b75404c88
RaDangelo <rberga95@gmail.com>
2020-08-15 20:03:28 -0300

    forms layout

--- libs/ui/src/lib/reactive-form/reactive-form.component.ts
+++ libs/ui/src/lib/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'angular-forms-reactive-form',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form.component.scss']
 })
 export class ReactiveFormComponent implements OnInit {
+  courseForm: FormGroup;
 
-  constructor() { }
+  constructor() {
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.initForm();
+  }
+
+  onSubmit() {
+    // Since we have  access to the FormGroup instance we can directly output the same
+    console.log(this.courseForm);
+  }
+
+  private initForm() {
+    this.courseForm = new FormGroup({
+      'courseName': new FormControl(null, Validators.required),
+      'courseDesc': new FormControl(null, [Validators.required, Validators.minLength(100)]),
+      'courseAmount': new FormControl(null)
+    });
   }
 
 }
